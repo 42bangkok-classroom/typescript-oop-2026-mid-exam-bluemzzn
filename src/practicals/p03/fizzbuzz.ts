4e5b7ea5b6cf9fdf1c5f32ff4ba227ee3f3cced7
Kuljira Namjai <synthiawithsyren@gmail.com>
2026-01-21 16:19:10 +0700

    p03 round 3

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -4,12 +4,14 @@ let fizzBuzz = process.argv[2];
 
 if (!fizzBuzz || fizzBuzz.trim() === "") {
     console.log("Invalid input");
+    process.exit();
 }
 
 
 let fizzBuzzInput = Number(fizzBuzz);
 if (Number.isNaN(fizzBuzzInput) || fizzBuzzInput < 0 || fizzBuzzInput > 100) {
     console.log("Invalid input");
+    
 }
 
 else {

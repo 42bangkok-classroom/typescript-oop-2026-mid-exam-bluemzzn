a982b5f93f13b1010152cc51192848d834c8e87d
Kuljira Namjai <synthiawithsyren@gmail.com>
2026-01-21 14:34:05 +0700

    p02 round 1

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,25 @@
 // Write your code below
+
+let grade = process.argv[2];
+
+if (!grade || Number.isNaN(grade) || grade.length <= 0 || grade.length >= 100) {
+    console.log("Invalid Input");
+}
+
+else {
+    let gradeInput = Number(grade);
+
+    if (gradeInput >= 80) {
+        console.log('A');
+    } else if (gradeInput >= 70) {
+        console.log('B');
+
+    }else if (gradeInput >= 60) {
+        console.log('C');
+
+    }else if (gradeInput >= 50) {
+        console.log('D');
+    } else{
+        console.log('F');
+    }
+}

f212dea12b83409ee22b753e0274605599d1e3fc
Kuljira Namjai <synthiawithsyren@gmail.com>
2026-01-21 16:36:53 +0700

    p06 round 1

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,5 +1,16 @@
 function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
   // Write your code below
+
+  let unique1 = arr1.filter((o) => arr2.indexOf(o) === -1);
+  let unique2 = arr2.filter((o) => arr1.indexOf(o) === -1);
+
+  const unique = unique1.concat(unique2);
+
+  if(unique){
+    return unique;
+  }else{
+    return [];
+  }
 }
 
 const arr1 = [1, 2, 3, 4];
